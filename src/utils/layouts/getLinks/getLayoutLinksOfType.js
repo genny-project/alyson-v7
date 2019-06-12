9e9e773f8be78a37ae37707f2288e770f78aa67f
Nischal Gautam <nischal1024@gmail.com>
2019-06-12 13:27:24 +1000

    removed sublayout checks

--- src/utils/layouts/getLinks/getLayoutLinksOfType.js
+++ src/utils/layouts/getLinks/getLayoutLinksOfType.js
@@ -7,9 +7,7 @@ const getLayoutLinksOfType = ( baseEntityCodes, layoutData, linkType ) => {
   if ( !isArray( baseEntityCodes, { ofMinLength: 1 })) return [];
 
   return baseEntityCodes.filter(
-    link =>
-      link.type === linkType &&
-      ( link.type === 'sublayout' || dlv( layoutData, `${link.type}s.${link.code}` ) != null )
+    link => link.type === linkType && dlv( layoutData, `${link.type}s.${link.code}` ) != null
   );
 };
 
