903ff2904feea726170b9aaca1bc58b6cc7b8c09
Nischal Gautam <nischal1024@gmail.com>
2019-06-21 15:32:23 +1000

    session check

--- src/views/components/display-session/DisplaySession.js
+++ src/views/components/display-session/DisplaySession.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
+import store from '../../../redux/store';
 
 export default function DisplaySession() {
   return (
-    <div style={{ padding: 10, background: '#e3e3e3' }}>
-      <pre>
+    <div style={{ padding: 10, background: '#e3e3e3', overflow: 'scroll' }}>
+      <pre style={{ width: '900px' }}>
+        <h1>
+          {' '}
+From Local Storage
+        </h1>
         <span>
 kcSessionStateState:
         </span>
@@ -17,6 +23,17 @@ kcAuth:
         <p style={{ color: 'green' }}>
           {JSON.stringify( localStorage.getItem( 'kcAuth' ))}
         </p>
+
+        <div style={{ height: 300, overflow: 'scroll', overflowY: 'auto ' }}>
+          <h1>
+            {' '}
+From Redux store
+          </h1>
+
+          <p>
+            <ReactJson src={store.getState().keycloak} />
+          </p>
+        </div>
       </pre>
     </div>
   );
