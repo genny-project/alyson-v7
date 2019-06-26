8330a10bd9b859ab9ab12661e3f3e3b27c4c9d88
Nischal Gautam <nischal1024@gmail.com>
2019-06-26 14:02:58 +1000

    logic for devMode

--- src/views/components/test-id-handler/TestIdHandler.js
+++ src/views/components/test-id-handler/TestIdHandler.js
@@ -1,5 +1,6 @@
 import React, { Component, isValidElement } from 'react';
 import { string, integer, node } from 'prop-types';
+import queryString from 'query-string';
 import { store } from '../../../redux';
 import * as actions from '../../../redux/actions';
 
@@ -10,51 +11,70 @@ class TestIdHandler extends Component {
   static defaultProps = {
     testID: 'test-id',
     timer: 300,
-  };
+  }
 
   static propTypes = {
     children: node,
     testID: string,
     timer: integer,
-  };
+  }
 
-  handleMouseOverDebounced = () => {
-    window.clearTimeout( timeoutMouseOutID );
+  // Check if devMode is supplied as a query string in the URL
+  isDebugMode() {
+    if ( window ) {
+      const paramsFromWindow = window.location.search;
+      const values = queryString.parse( paramsFromWindow );
 
-    timeoutMouseOverID = window.setTimeout(() => {
-      this.handleMouseOver();
-    }, this.props.timer );
-  };
+      if ( values && values.devMode === 'true' ) {
+        store.dispatch( actions.setTestId({ id: this.props.testID }));
+      }
+    }
+  }
 
   handleMouseOutDebounced = () => {
     window.clearTimeout( timeoutMouseOverID );
 
-    timeoutMouseOutID = window.setTimeout(() => {
-      this.handleMouseOut();
-    }, this.props.timer );
-  };
+    timeoutMouseOutID = window.setTimeout(
+      () => {
+        this.handleMouseOut();
+      },
+      this.props.timer,
+    );
+  }
+
+  handleMouseOverDebounced = () => {
+    window.clearTimeout( timeoutMouseOutID );
+
+    timeoutMouseOverID = window.setTimeout(
+      () => {
+        this.handleMouseOver();
+      },
+      this.props.timer,
+    );
+  }
 
   handleMouseOver = () => {
-    store.dispatch( actions.setTestId({ id: this.props.testID }));
-  };
+    this.isDebugMode ? store.dispatch( actions.setTestId({ id: this.props.testID })) : null;
+  }
 
   handleMouseOut = () => {
-    store.dispatch( actions.removeTestId());
-  };
+    this.isDebugMode ? store.dispatch( actions.removeTestId()) : null;
+  }
 
   render() {
-    const { children } = this.props;
-
-    return React.Children.map(
+    const {
       children,
-      child =>
+    } = this.props;
+
+    return (
+      React.Children.map( children, child => (
         isValidElement( child )
           ? React.cloneElement( child, {
             ...child.props,
             onMouseOver: this.handleMouseOverDebounced,
             onMouseOut: this.handleMouseOutDebounced,
-          })
-          : null
+          }) : null
+      ))
     );
   }
 }
