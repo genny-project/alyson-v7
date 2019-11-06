37d981956b20360ffe55ebbc1d4fb0c550fbe447
baradghimire <baradghimire@gmail.com>
2019-11-06 14:06:23 +1100

    changes to Unity.js component

--- src/views/pages/generic/Generic.js
+++ src/views/pages/generic/Generic.js
@@ -4,7 +4,7 @@ import { object } from 'prop-types';
 // import queryString from 'query-string';
 // import { location } from '../../../utils';
 import { KeycloakConsumer, Redirect, Frame, Box, ActivityIndicator } from '../../components';
-import Unity from '../../components/input/unity'
+import Unity from '../../components/input/unity';
 
 class Generic extends Component {
   static propTypes = {
