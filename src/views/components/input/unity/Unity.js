4247d62208194c70b408dc8334b895c7769f2007
baradghimire <baradghimire@gmail.com>
2019-11-08 13:30:37 +1100

    added ability to switch Unity scenes from React

--- src/views/components/input/unity/Unity.js
+++ src/views/components/input/unity/Unity.js
@@ -2,22 +2,30 @@ import React from 'react';
 import Unity, { UnityContent } from 'react-unity-webgl';
 
 class UnityComponent extends React.Component {
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
     this.unityContent = new UnityContent(
       '/unity/unison_webgl.json',
       '/unity/UnityLoader.js'
     );
-    this.unityContent.on( 'progress', progression => {
+    this.unityContent.on('progress', progression => {
       this.setState({
         progression: progression,
       });
     });
-    this.unityContent.on( 'ObjectClick', objectname => {
+    this.unityContent.on('ObjectClick', objectname => {
       this.setState({
         objectname: objectname,
       });
     });
+  }
+
+  onClick = (SceneIndex) => {
+    this.unityContent.send(
+      "Main Camera",
+      "ChangeScene",
+      SceneIndex
+    );
   }
 
   state = {
@@ -35,9 +43,18 @@ class UnityComponent extends React.Component {
           {`Object pressed: ${this.state.objectname}`}
         </div>
         <div>
+          <button onClick={() => this.onClick('0')}>World View</button>
+          <button onClick={() => this.onClick('1')}>Scene 1</button>
+          <button onClick={() => this.onClick('2')}>Scene 2</button>
+          <button onClick={() => this.onClick('3')}>Scene 3</button>
+          <button onClick={() => this.onClick('4')}>Scene 4</button>
+
+        </div>
+        <div>
           <Unity unityContent={this.unityContent} />
         </div>
-      </div> );
+
+      </div>);
   }
 }
 
