3cf90f53dee558d4e0c5f6cf6a771726196bcc6f
Jack Li <hbjoylee@gmail.com>
2019-11-12 10:58:03 +1100

    Update Dockerfile, build-docker.sh and push.sh
    Dockerfile
    Add Security audit
    
    build-docker.sh
    Add clean up images
    
    push.sh
    Add double quote

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+project='alyson-v7'
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+commit_hash=$(git rev-parse HEAD)
+
 if [ -z "${1}" ]; then
-   version="latest"
+  version="latest"
 else
-   version="${1}"
+  version="${1}"
 fi
 
-docker build -t gennyproject/alyson-v7:${version} .
+# build docker imges
+echo "Git branch:" "${branch}"
+echo "Last git commit hash:" "${commit_hash}"
+docker build -t gennyproject/${project}:"${version}" .
+
+#clean up
+image_ids=$(docker images | grep ${project} | grep none)
+if [ "${image_ids:-0}" == 0 ]; then
+  echo 'Skip clean up'
+else
+  docker images | grep ${project} | grep none | awk '{print $3}' | xargs docker rmi
+fi
--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ else
 fi
 
 
-docker push gennyproject/alyson-v7:${version}
-docker tag  gennyproject/alyson-v7:${version} gennyproject/alyson-v7:latest
+docker push gennyproject/alyson-v7:"${version}"
+docker tag  gennyproject/alyson-v7:"${version}" gennyproject/alyson-v7:latest
 docker push gennyproject/alyson-v7:latest
 
