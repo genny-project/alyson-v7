ad98baaa611c5671cc904bcf29c8df8ca76a6af2
baradghimire <baradghimire@gmail.com>
2019-11-12 14:33:55 +1100

    changed .unityweb files URL

--- src/views/components/input/unity/Unity.js
+++ src/views/components/input/unity/Unity.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import Unity, { UnityContent } from 'react-unity-webgl';
-import queryString from 'query-string';
 
 class UnityComponent extends React.Component {
   constructor(props) {
     super(props);
-
-    // creat an obj
-
-    // let foo = {
-    // dataUrl
-    //wasmCodeUrl
-    //wasmFrameworkUrl
-    //}
     this.unityContent = new UnityContent(
       '/unity/unison_webgl.json',
       '/unity/UnityLoader.js'
-      // queryString.parse(('https://raw.githubusercontent.com/genny-project/alyson-v7/baradUnity/public/unity/unison_webgl.json')),
-      // 'https://raw.githubusercontent.com/genny-project/alyson-v7/baradUnity/public/unity/UnityLoader.js'
     );
+
     this.unityContent.on('progress', progression => {
       this.setState({
         progression: progression,
       });
     });
+
     this.unityContent.on('ObjectClick', objectname => {
       this.setState({
         objectname: objectname,
@@ -48,10 +39,10 @@ class UnityComponent extends React.Component {
     return (
       <div>
         <div>
-          {`Loading ${this.state.progression * 100}%`}
+          {`${this.state.progression * 100}%`}
         </div>
         <div>
-          {`Object pressed: ${this.state.objectname}`}
+          {`Unity click: ${this.state.objectname}`}
         </div>
         <div>
           <button onClick={() => this.onClick('0')}>World View</button>
@@ -59,12 +50,10 @@ class UnityComponent extends React.Component {
           <button onClick={() => this.onClick('2')}>Scene 2</button>
           <button onClick={() => this.onClick('3')}>Scene 3</button>
           <button onClick={() => this.onClick('4')}>Scene 4</button>
-
         </div>
         <div>
           <Unity unityContent={this.unityContent} />
         </div>
-
       </div>);
   }
 }
