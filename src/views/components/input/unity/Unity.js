b732fbc1b62d65ced193c75d450c1602dd4af55f
c7yrus <c7yrus@gmail.com>
2019-11-18 10:58:19 +1100

    added the methods to send the event as both answer and button click from the same component

--- src/views/components/input/unity/Unity.js
+++ src/views/components/input/unity/Unity.js
@@ -1,32 +1,63 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import Unity, { UnityContent } from 'react-unity-webgl';
-import Proptypes from 'prop-types';
+import { store } from '../../../../redux' ;
+import { Bridge } from '../../../../utils';
 
 class UnityComponent extends React.Component {
-  static propTypes = {
-    onChangeValue: Proptypes.func,
-  }
-
   constructor( props ) {
     super( props );
     this.unityContent = new UnityContent(
       '/unity/unison_webgl.json',
       '/unity/UnityLoader.js'
     );
+
     this.unityContent.on( 'progress', progression => {
       this.setState({
         progression: progression,
       });
     });
-    this.unityContent.on( 'ObjectClick', objectname => {
+
+    this.unityContent.on( 'sendEvent', ( params ) => {
+      this.handleEvent( params );
+    });
+
+    this.unityContent.on( 'sendAnswer', ( params ) => {
       // console.log( 'insideunitycontent' );
-      this.handleChange( objectname );
+      this.handleChange( params );
     });
   }
 
   state = {
     progression: null,
     objectname: 'none',
+  }
+
+  handleEvent = ( event ) => {
+    const { code } = this.props.ask.question;
+    const { parentGroupCode } = this.props;
+    const { disabled } = this.props.ask;
+
+    console.log( 'thisobject:', this );
+
+    if ( disabled ) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      return false;
+    }
+
+    if (
+      code &&
+      parentGroupCode
+    ) {
+      Bridge.sendFormattedEvent(
+        {
+          code,
+          parentCode: parentGroupCode,
+        }
+      );
+    }
   }
 
   handleChange = ( event ) => {
