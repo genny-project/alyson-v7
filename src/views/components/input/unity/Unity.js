149c54d1a85a8679f43c47794e691d1d57c68579
c7yrus <c7yrus@gmail.com>
2019-11-19 10:23:04 +1100

    eslint fix

--- src/views/components/input/unity/Unity.js
+++ src/views/components/input/unity/Unity.js
@@ -4,21 +4,19 @@ import Unity, { UnityContent } from 'react-unity-webgl';
 import { Bridge } from '../../../../utils';
 
 class UnityComponent extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor( props ) {
+    super( props );
     this.unityContent = new UnityContent(
-      //'/unity/unison_webgl_genny.json',
+      // '/unity/unison_webgl_genny.json',
       '/unity/unison_webgl.json',
       '/unity/UnityLoader.js'
     );
 
     this.unityContent.on( 'progress', progression => {
-
       this.setState({
         progression: progression,
       });
     });
-
 
     this.unityContent.on( 'unityEvent', ( params ) => {
       this.handleEvent( params );
@@ -27,22 +25,21 @@ class UnityComponent extends React.Component {
     this.unityContent.on( 'unityAnswer', ( params ) => {
       // console.log( 'insideunitycontent' );
       this.handleChange( params );
-
     });
-  }
-
-  onClick = (SceneIndex) => {
-    this.unityContent.send(
-      "Main Camera",
-      "ChangeScene",
-      SceneIndex
-    );
   }
 
   state = {
     progression: null,
     eventname: 'none',
     answername: 'none',
+  }
+
+  onClick = ( SceneIndex ) => {
+    this.unityContent.send(
+      'Main Camera',
+      'ChangeScene',
+      SceneIndex
+    );
   }
 
   handleEvent = ( event ) => {
@@ -91,16 +88,26 @@ class UnityComponent extends React.Component {
           {`UnityAnswer: ${this.state.answername}`}
         </div>
         <div>
-          <button onClick={() => this.onClick('0')}>World View</button>
-          <button onClick={() => this.onClick('1')}>Scene 1</button>
-          <button onClick={() => this.onClick('2')}>Scene 2</button>
-          <button onClick={() => this.onClick('3')}>Scene 3</button>
-          <button onClick={() => this.onClick('4')}>Scene 4</button>
+          <button onClick={() => this.onClick( '0' )}>
+World View
+                    </button>
+          <button onClick={() => this.onClick( '1' )}>
+Scene 1
+                    </button>
+          <button onClick={() => this.onClick( '2' )}>
+Scene 2
+                    </button>
+          <button onClick={() => this.onClick( '3' )}>
+Scene 3
+                    </button>
+          <button onClick={() => this.onClick( '4' )}>
+Scene 4
+                    </button>
         </div>
         <div>
           <Unity unityContent={this.unityContent} />
         </div>
-      </div>);
+      </div> );
   }
 }
 
