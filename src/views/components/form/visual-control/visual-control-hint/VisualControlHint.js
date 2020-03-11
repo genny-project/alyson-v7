ea21252d950e4ecf01df22467f51469a622e3a45
c7yrus <c7yrus@gmail.com>
2020-03-11 16:03:11 +1100

    passing the question code tot the tooltip component as a children

--- src/views/components/form/visual-control/visual-control-hint/VisualControlHint.js
+++ src/views/components/form/visual-control/visual-control-hint/VisualControlHint.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { string } from 'prop-types';
 // import { string, object, bool, number, func } from 'prop-types';
 // import { isArray } from '../../../../../../utils';
 import { Box, Icon } from '../../../../components';
+import Tooltip from '../../../../components/tooltip';
 
 class VisualControlHint extends Component {
   static propTypes = {
+    questionCode: string,
   }
 
   render() {
@@ -19,13 +22,15 @@ class VisualControlHint extends Component {
         cursor="pointer"
         {...restProps}
       >
-        <Icon
-          name="arrow_up"
-          size="xs"
-          color="grey"
-          cursor="help"
-          {...restProps}
-        />
+        <Tooltip text={`${this.props.questionCode}`}>
+          <Icon
+            name="help"
+            size="xs"
+            color="grey"
+            cursor="help"
+            {...restProps}
+          />
+        </Tooltip>
       </Box>
     );
   }
