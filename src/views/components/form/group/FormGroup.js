76dcd6c55c38bfbe7ae18217e5b7f4a4ad38c581
conorpirie <conor.pirie@gmail.com>
2020-03-18 14:51:25 +1100

    adding more information to test ids

--- src/views/components/form/group/FormGroup.js
+++ src/views/components/form/group/FormGroup.js
@@ -632,7 +632,7 @@ class FormGroup extends Component {
                 <WrapperElement
                   subcomponentProps={subcomponentProps}
                   isClosed={this.props.isClosed}
-                  testID={`${parentGroupCode || questionCode}:${questionCode}`}
+                  testID={`${parentGroupCode || questionCode}:${questionCode}${targetCode ? `:${targetCode}` : ''}`}
                   showIcon={(
                     properties.renderQuestionGroupIcon != null
                       ? properties.renderQuestionGroupIcon
