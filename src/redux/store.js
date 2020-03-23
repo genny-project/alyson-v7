e60dccd4e0f475a2769c42c7b752f24afac063b6
conorpirie <conor.pirie@gmail.com>
2020-03-23 12:46:24 +1100

    updating redux store with a persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,31 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
 import middleware from './middleware';
 import reducers from './reducers';
 
-const store = createStore(
-  reducers,
-  middleware
-);
+// const store = createStore(
+//   reducers,
+//   middleware
+// );
 
-export default store;
+// export default store;
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer( persistConfig, reducers );
+
+const setUp = () => {
+  const store = createStore( persistedReducer, middleware );
+  const persistor = persistStore( store );
+
+  return { store, persistor };
+};
+
+export const persistor = setUp().persistor;
+
+export default setUp().store;
