c909ab818d042b72f043729e2ff1c599c5fd3a7e
conorpirie <conor.pirie@gmail.com>
2020-03-25 13:48:31 +1100

    adding checks to vertx, testing transformations

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createTransform } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import middleware from './middleware';
@@ -12,9 +12,28 @@ import reducers from './reducers';
 
 // export default store;
 
+const SetTransform = createTransform(
+  // transform state on its way to being serialized and persisted.
+  ( inboundState, key ) => {
+    // convert mySet to an Array.
+    console.warn( 'inbound', key, JSON.stringify( inboundState ));
+    // debugger; // eslint-disable-line
+
+    return { ...inboundState };
+  },
+  // transform state being rehydrated
+  ( outboundState, key ) => {
+    // convert mySet back to a Set.
+    console.warn( 'outbound', key, JSON.stringify( outboundState ));
+
+    return { ...outboundState };
+  },
+);
+
 const persistConfig = {
   key: 'root',
   storage,
+  // transforms: [SetTransform],
 };
 
 const persistedReducer = persistReducer( persistConfig, reducers );
