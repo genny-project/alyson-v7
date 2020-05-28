be099fd0fe6eb904f2b00f4f61512058de324305
c7yrus <c7yrus@gmail.com>
2020-05-28 12:36:26 +1000

    removing boxes for styling changes

--- src/views/components/input/date-time/time-picker/InputTimePicker.web.js
+++ src/views/components/input/date-time/time-picker/InputTimePicker.web.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-prop-types */
 /**
  * Adapted from https://codesandbox.io/s/j47zv28xkw
  */
@@ -6,22 +7,18 @@ import React, { PureComponent } from 'react';
 import { string, func, oneOfType, object, bool, shape } from 'prop-types';
 import { getHours, getMinutes } from 'date-fns';
 import { Input, Box,Fragment } from '../../../../components';
-import { getDeviceSize } from '../../../../../utils';
 import DateTimeBase from '../date-time-base';
 
 class InputDatePicker extends PureComponent {
   static defaultProps = {
     displayFormat: 'hh:mm A',
-    calendarHeaderColor: 'white',
-    calendarHeaderTextColor: 'black',
     placeholder: 'Please select a date...', // eslint-disable-line
     // placeholder: 'day month, year',
   }
 
   static propTypes = {
     displayFormat: string,
-    calendarHeaderColor: string,
-    calendarHeaderTextColor: string,
+
     onChangeValue: func,
     value: oneOfType( [string, object] ),
     testID: string,
@@ -74,146 +71,109 @@ class InputDatePicker extends PureComponent {
           selectHour,
           selectMinute,
           selectAmPm,
-          componentProps,
           updateState,
         }) => {
           return (
             <Fragment>
-              <Box
-                flexDirection="column"
-                marginTop="0.5rem"
-                shadowColor="#000"
-                shadowOpacity={0.4}
-                shadowRadius={5}
-                shadowOffset={{
-                  width: 0,
-                  height: 0,
-                }}
-                // borderRadius={5}
-                overflow="hidden"
-                backgroundColor="#FFF"
-                position="absolute"
-                paddingY={5}
-                zIndex={20}
-                top="100%"
-                {...( getDeviceSize() === 'sm' ? { width: '100%' } : null )}
-                onPress={event => {
-                  event.stopPropagation();
-                }}
-                componentID="INPUT-ITEM-WRAPPER"
-                {...componentProps['input-item-wrapper']}
-              >
-                <Box
-                  justifyContent="space-between"
-                  alignItems="center"
-                  padding={5}
-                  onPress={event => {
-                    event.stopPropagation();
-                  }}
-                  zIndex={1}
-                  width="100%"
-                >
-
-                  {/*  ----------------------------
+              <Box>
+                {/*  ----------------------------
                             HOUR DROPDOWN
                     ------------------------------*/}
 
-                  <Box
-                    flex={1}
-                  >
-                    <Input
-                      type="dropdown"
-                      items={hours.map(( hour, index ) => (
-                        { value: hour, label: hour, weight: index }
-                      ))}
-                      identifier="TIMEDROPDOWN"
-                      sortByWeight
-                      onChangeValue={selectHour( setDate, date )}
-                      color="#000"
-                      backgroundColor="#FFF"
-                      padding={5}
-                      borderRadius={10}
-                      borderWidth={0}
-                      textAlign="center"
-                      cursor="pointer"
-                      testID={`input-date-picker-hour ${testID}`}
-                      nonTabable
-                      editable
-                      placeholder={selectedItem
-                        ? getHours( selectedItem ) > 9
-                          ? getHours( selectedItem ) > 12 ? `0${getHours( selectedItem ) - 12}` : getHours( selectedItem )
-                          : `0${getHours( selectedItem )}`
-                        : 'Hours'}
-                      onChangeState={updateState( 'input-field' )}
-                      value={selectedItem}
-                    />
-                  </Box>
+                <Box
+                  flex={1}
+                >
+                  <Input
+                    type="dropdown"
+                    items={hours.map(( hour, index ) => (
+                      { value: hour, label: hour, weight: index }
+                    ))}
+                    identifier="TIMEDROPDOWN"
+                    sortByWeight
+                    onChangeValue={selectHour( setDate, date )}
+                    color="#000"
+                    backgroundColor="#FFF"
+                    padding={5}
+                    borderRadius={10}
+                    borderWidth={0}
+                    textAlign="center"
+                    cursor="pointer"
+                    testID={`input-date-picker-hour ${testID}`}
+                    nonTabable
+                    editable
+                    placeholder={selectedItem
+                      ? getHours( selectedItem ) > 9
+                        ? getHours( selectedItem ) > 12 ? `0${getHours( selectedItem ) - 12}` : getHours( selectedItem )
+                        : `0${getHours( selectedItem )}`
+                      : 'Hours'}
+                    onChangeState={updateState( 'input-field' )}
+                    value={selectedItem}
+                  />
+                </Box>
 
-                  {/*  ----------------------------
+                {/*  ----------------------------
                             MINUTES DROPDOWN
                     ------------------------------*/}
 
-                  <Box
-                    flex={1}
-                  >
-                    <Input
-                      type="dropdown"
-                      items={minutes.map(( minute, index ) => (
-                        { value: minute, label: minute, weight: index }
-                      ))}
-                      identifier="MINUTEDROPDOWN"
-                      sortByWeight
-                      onChangeValue={selectMinute( setDate, date )}
-                      color="#000"
-                      backgroundColor="#FFF"
-                      padding={5}
-                      borderRadius={10}
-                      borderWidth={0}
-                      textAlign="center"
-                      cursor="pointer"
-                      testID={`input-date-picker-minute ${testID}`}
-                      nonTabable
-                      editable
-                      placeholder={selectedItem
-                        ? getMinutes( selectedItem ) > 9
-                          ? getMinutes( selectedItem )
-                          : `0${getMinutes( selectedItem )}`
-                        : 'Minutes'}
-                      onChangeState={updateState( 'input-field' )}
-                    />
-                  </Box>
+                <Box
+                  flex={1}
+                >
+                  <Input
+                    type="dropdown"
+                    items={minutes.map(( minute, index ) => (
+                      { value: minute, label: minute, weight: index }
+                    ))}
+                    identifier="MINUTEDROPDOWN"
+                    sortByWeight
+                    onChangeValue={selectMinute( setDate, date )}
+                    color="#000"
+                    backgroundColor="#FFF"
+                    padding={5}
+                    borderRadius={10}
+                    borderWidth={0}
+                    textAlign="center"
+                    cursor="pointer"
+                    testID={`input-date-picker-minute ${testID}`}
+                    nonTabable
+                    editable
+                    placeholder={selectedItem
+                      ? getMinutes( selectedItem ) > 9
+                        ? getMinutes( selectedItem )
+                        : `0${getMinutes( selectedItem )}`
+                      : 'Minutes'}
+                    onChangeState={updateState( 'input-field' )}
+                  />
+                </Box>
 
-                  {/*  ----------------------------
+                {/*  ----------------------------
                             AMPM DROPDOWN
                     ------------------------------*/}
 
-                  <Box
-                    flex={1}
-                  >
-                    <Input
-                      type="dropdown"
-                      items={clockFormat.map(( format, index ) => (
-                        { value: format, label: format, weight: index }
-                      ))}
-                      onChangeValue={selectAmPm( setDate, date )}
-                      color="#000"
-                      backgroundColor="#FFF"
-                      padding={5}
-                      borderRadius={10}
-                      borderWidth={0}
-                      textAlign="center"
-                      cursor="pointer"
-                      testID={`input-date-picker-ampm ${testID}`}
-                      nonTabable
-                      editable
-                      placeholder={selectedItem
-                        ?  getHours( selectedItem ) < 12 ? 'Am' : 'Pm'
-                        : 'Am / Pm'
+                <Box
+                  flex={1}
+                >
+                  <Input
+                    type="dropdown"
+                    items={clockFormat.map(( format, index ) => (
+                      { value: format, label: format, weight: index }
+                    ))}
+                    onChangeValue={selectAmPm( setDate, date )}
+                    color="#000"
+                    backgroundColor="#FFF"
+                    padding={5}
+                    borderRadius={10}
+                    borderWidth={0}
+                    textAlign="center"
+                    cursor="pointer"
+                    testID={`input-date-picker-ampm ${testID}`}
+                    nonTabable
+                    editable
+                    placeholder={selectedItem
+                      ?  getHours( selectedItem ) < 12 ? 'Am' : 'Pm'
+                      : 'Am / Pm'
                         }
-                    />
-                  </Box>
+                  />
                 </Box>
-
               </Box>
             </Fragment>
           );
