b62018e2acc7d2909e81f25b56d8746d371e736f
Sam Levens <sam@eventhub.com.au>
2020-06-03 16:57:11 +1000

    add-more-details

--- src/views/components/frame/sam/views/details/index.js
+++ src/views/components/frame/sam/views/details/index.js
@@ -1,84 +1,135 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { compose, prop, path, toUpper } from 'ramda';
 
 import { Grid, Typography, Avatar } from '@material-ui/core';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Details = ({ attributes, targetCode }) => {
   const detailView = prop( targetCode, attributes );
 
-  const print = prop => path( [`PRI_${toUpper( prop )}`, 'value'], detailView );
+  const print = prop => path( [`PRI_${toUpper( prop )}`, 'value'], detailView ) || '';
+
+  const [rating, setRating] = useState( 0 );
+  const classes = useStyles();
 
   return (
     <Grid
       container
       direction="column"
-      spacing={2}
+      spacing={4}
+      className={classes.detailsContainer}
     >
       <Grid item>
         <Typography variant="h5">
-Details
+          {print( 'name' )}
         </Typography>
       </Grid>
-      <Grid
-        container
-        direction="row"
-        spacing={2}
-      >
-        <Grid item>
-          <Avatar
-            alt={print( 'name' )}
-            src={print( 'image_url' )}
-          />
-        </Grid>
-        <Grid item>
-          <Grid
-            container
-            direction="column"
-            spacing={2}
-          >
-            <Grid item>
-              <Typography>
-                {print( 'name' )}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography>
-                {print( 'address_full' )}
-              </Typography>
+      <Grid item>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          alignItems="center"
+        >
+          <Grid item>
+            <Avatar
+              alt={print( 'name' )}
+              src={print( 'image_url' )}
+            />
+          </Grid>
+          <Grid item>
+            <Grid
+              container
+              direction="column"
+            >
+              <Grid item>
+                <Typography>
+                  {print( 'address_full' )}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography>
+                  {`${print( 'email' )}`}
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
       <Grid item>
+        <Typography className={classes.label}>
+          {'Mobile'}
+        </Typography>
         <Typography>
-          {`Email - ${print( 'email' )}`}
+          {print( 'mobile' )}
         </Typography>
       </Grid>
       <Grid item>
+        <Typography className={classes.label}>
+          {'Industry'}
+        </Typography>
         <Typography>
-          {`Gender - ${print( 'gender' )}`}
+          {print( 'industry' )}
         </Typography>
       </Grid>
-      <Grid>
-        {print( 'is_supervisor' ) ? (
-          <Typography>
-Supervisor
-          </Typography>
-        ) : null}
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Education Provider'}
+        </Typography>
+        <Typography>
+          {print( 'education_provider' )}
+        </Typography>
       </Grid>
-      <Grid>
-        {print( 'is_admin' ) ? (
-          <Typography>
-Admin
-          </Typography>
-        ) : null}
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Current Course'}
+        </Typography>
+        <Typography>
+          {print( 'current_course' )}
+        </Typography>
       </Grid>
-      <Grid>
-        {print( 'is_agent' ) ? (
-          <Typography>
-Agent
-          </Typography>
-        ) : null}
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Specialisation'}
+        </Typography>
+        <Typography>
+          {print( 'specialisation' )}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Rating'}
+        </Typography>
+        <Rating
+          value={rating}
+          onChange={( event, newValue ) => setValue( newValue )}
+        />
+      </Grid>
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Transport Options'}
+        </Typography>
+        <Typography>
+          {print( 'transport_options' )}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Region'}
+        </Typography>
+        <Typography>
+          {print( 'region' )}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography className={classes.label}>
+          {'Next Scheduled Interview'}
+        </Typography>
+        <Typography>
+          {print( 'interview' )}
+        </Typography>
       </Grid>
     </Grid>
   );
---
+++ src/views/components/frame/sam/views/details/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles( theme => ({
+  detailsContainer: {
+    padding: theme.spacing( 2 ),
+  },
+  label: {
+    fontWeight: 500,
+  },
+}));
+
+export default useStyles;
