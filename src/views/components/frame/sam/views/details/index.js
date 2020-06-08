17e16c3fea30472d79fda00c08d8de53b0c47c2f
Sam Levens <sam@eventhub.com.au>
2020-06-08 18:07:22 +1000

    internship-details-page-to-be-built

--- src/views/components/frame/sam/views/details/index.js
+++ src/views/components/frame/sam/views/details/index.js
@@ -1,197 +1,85 @@
 import React, { useState } from 'react';
-import { prop, path, toUpper } from 'ramda';
+import { prop, path, toUpper, contains, map } from 'ramda';
 
 import { Grid, Typography, Avatar } from '@material-ui/core';
 import { Rating } from '@material-ui/lab';
 
 import useStyles from './styles';
 
+const RowItem = ({ label, code, classes, print, rating, setRating }) => (
+  <Grid item>
+    <Grid container direction="row">
+      <Typography className={classes.label}>{label}</Typography>
+      {code === 'rating' ? (
+        <Rating name="rating" value={rating} onChange={(event, newValue) => setRating(newValue)} />
+      ) : (
+        <Typography>{print(code)}</Typography>
+      )}
+    </Grid>
+  </Grid>
+);
+
+const printPer = [
+  { label: 'Mobile', code: 'mobile' },
+  { label: 'Industry', code: 'assoc_industry' },
+  { label: 'Education Provider', code: 'assoc_ep' },
+  { label: 'Current Course', code: 'current_course' },
+  { label: 'Student Id', code: 'student_id' },
+  { label: 'Specialisation', code: 'assoc_specialisation' },
+  { label: 'Rating', code: 'rating' },
+  { label: 'Transport Options', code: 'transport_options' },
+  { label: 'Region', code: 'region' },
+  { label: 'Next Scheduled Interview', code: 'next_interview' },
+];
+
+const printBeg = [];
+
 const Details = ({ attributes, targetCode }) => {
-  const detailView = prop( targetCode, attributes );
+  const detailView = prop(targetCode, attributes);
 
-  const print = prop => path( [`PRI_${toUpper( prop )}`, 'value'], detailView ) || '';
+  const print = prop => path([`PRI_${toUpper(prop)}`, 'value'], detailView) || '';
 
-  const [rating, setRating] = useState( 0 );
+  const [rating, setRating] = useState(0);
   const classes = useStyles();
 
-  console.log( detailView );
+  console.log(targetCode);
 
   return (
-    <Grid
-      container
-      direction="column"
-      spacing={4}
-      className={classes.detailsContainer}
-    >
+    <Grid container direction="column" spacing={4} className={classes.detailsContainer}>
       <Grid item>
-        <Typography variant="h5">
-          {print( 'name' )}
-        </Typography>
+        <Typography variant="h5">{print('name')}</Typography>
       </Grid>
       <Grid item>
-        <Grid
-          container
-          direction="row"
-          spacing={2}
-          alignItems="center"
-        >
+        <Grid container direction="row" spacing={2} alignItems="center">
           <Grid item>
-            <Avatar
-              alt={print( 'name' )}
-              src={print( 'user_profile_picture' )}
-            />
+            <Avatar alt={print('name')} src={print('user_profile_picture')} />
           </Grid>
           <Grid item>
-            <Grid
-              container
-              direction="column"
-            >
+            <Grid container direction="column">
               <Grid item>
-                <Typography>
-                  {print( 'address_full' )}
-                </Typography>
+                <Typography>{print('address_full')}</Typography>
               </Grid>
               <Grid item>
-                <Typography>
-                  {`${print( 'email' )}`}
-                </Typography>
+                <Typography>{`${print('email')}`}</Typography>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Mobile'}
-          </Typography>
-          <Typography>
-            {print( 'mobile' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Industry'}
-          </Typography>
-          <Typography>
-            {print( 'industry' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Education Provider'}
-          </Typography>
-          <Typography>
-            {print( 'assoc_ep' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Current Course'}
-          </Typography>
-          <Typography>
-            {print( 'current_course' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Student Id'}
-          </Typography>
-          <Typography>
-            {print( 'student_id' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Specialisation'}
-          </Typography>
-          <Typography>
-            {print( 'specialisation' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Rating'}
-          </Typography>
-          <Rating
-            name="rating"
-            value={rating}
-            onChange={( event, newValue ) => setRating( newValue )}
+      {map(
+        ({ label, code }) => (
+          <RowItem
+            key={code}
+            label={label}
+            code={code}
+            print={print}
+            rating={rating}
+            setRating={setRating}
+            classes={classes}
           />
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Transport Options'}
-          </Typography>
-          <Typography>
-            {print( 'transport_options' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Region'}
-          </Typography>
-          <Typography>
-            {print( 'region' )}
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-        >
-          <Typography className={classes.label}>
-            {'Next Scheduled Interview'}
-          </Typography>
-          <Typography>
-            {print( 'interview' )}
-          </Typography>
-        </Grid>
-      </Grid>
+        ),
+        contains('PER', targetCode) ? printPer : printBeg
+      )}
     </Grid>
   );
 };
