f6393c91aeccef8b374b7a8fc3b1a098e49a041c
Sam Levens <sjlevens@gmail.com>
2020-06-19 11:08:49 +1000

    fix up css again

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AuthenticatedApp from './authenticated'
 import Routing from './routing'
+import { CssBaseline } from '@material-ui/core'
 
 const App = () => (
   <AuthenticatedApp>
+    <CssBaseline />
     <Routing />
   </AuthenticatedApp>
 )
