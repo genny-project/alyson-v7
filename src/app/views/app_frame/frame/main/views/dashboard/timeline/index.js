050accd5edd01be7d474b3f413763c64f6dffffa
c7yrus <c7yrus@gmail.com>
2020-07-03 12:12:55 +1000

    changes to the timeline component

--- src/app/views/app_frame/frame/main/views/dashboard/timeline/index.js
+++ src/app/views/app_frame/frame/main/views/dashboard/timeline/index.js
@@ -7,12 +7,13 @@ import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import HotelIcon from '@material-ui/icons/Hotel';
-import RepeatIcon from '@material-ui/icons/Repeat';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import TouchAppOutlinedIcon from '@material-ui/icons/TouchAppOutlined';
+import WorkOutlineOutlinedIcon from '@material-ui/icons/WorkOutlineOutlined';
+import AssignmentTurnedInOutlinedIcon from '@material-ui/icons/AssignmentTurnedInOutlined';
+import HowToRegOutlinedIcon from '@material-ui/icons/HowToRegOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 
 import useStyles from './styles'
 
@@ -26,67 +27,81 @@ const Timelines = () => {
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot>
-            <FastfoodIcon />
+            <HowToRegOutlinedIcon />
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <Paper elevation={3} className={classes.paper}>
             <Typography variant="h6" component="h1">
-              Register & Complete Profile
+              Register
             </Typography>
+            <Typography>Register in Internmatch</Typography>
+            <Typography>Complete Profile</Typography>
+            <Typography>Record Introduction Video</Typography>
           </Paper>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot color="primary">
-            <LaptopMacIcon />
+            <SearchOutlinedIcon />
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <Paper elevation={3} className={classes.paper}>
             <Typography variant="h6" component="h1">
-              Search Internships
+              Search
             </Typography>
+            <Typography>Search from recommended Internship Opportunities</Typography>
+            <Typography>Save Opportunities</Typography>
+            <Typography>Complete Accreditation</Typography>
           </Paper>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
+            <TouchAppOutlinedIcon />
           </TimelineDot>
           <TimelineConnector className={classes.secondaryTail} />
         </TimelineSeparator>
         <TimelineContent>
           <Paper elevation={3} className={classes.paper}>
             <Typography variant="h6" component="h1">
-              Apply for Internships
+              Apply
             </Typography>
+            <Typography>Applying for internships</Typography>
+            <Typography>Recommended skill development</Typography>
+            <Typography>Application sent to Host Company</Typography>
+            <Typography>Attend Interviews</Typography>
+            <Typography> Accept offer</Typography>
           </Paper>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot color="secondary">
-            <RepeatIcon />
+            <WorkOutlineOutlinedIcon />
           </TimelineDot>
           <TimelineConnector className={classes.secondaryTail} />
         </TimelineSeparator>
         <TimelineContent>
           <Paper elevation={3} className={classes.paper}>
             <Typography variant="h6" component="h1">
-              Undertake Internships
+              Internships
             </Typography>
+            <Typography>Induction</Typography>
+            <Typography>Progress reporting</Typography>
+            <Typography>Daily Logbook Completion</Typography>
           </Paper>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot color="secondary">
-            <RepeatIcon />
+          <TimelineDot color="primary">
+            <AssignmentTurnedInOutlinedIcon />
           </TimelineDot>
         </TimelineSeparator>
         <TimelineContent>
@@ -94,11 +109,12 @@ const Timelines = () => {
             <Typography variant="h6" component="h1">
               Work Ready
             </Typography>
+            <Typography>Receive Certificate</Typography>
+            <Typography>Update your LinkedIn Profile & Resume</Typography>
+            <Typography>Ask for a recommendation</Typography>
           </Paper>
         </TimelineContent>
       </TimelineItem>
-
-
     </Timeline>
   )
 
