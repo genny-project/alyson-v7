29ff7ea627f1d290e81336638be88dec201bb8cb
Barad Ghimire <baradghimire@gmail.com>
2020-07-07 14:28:57 +1000

    Combine scripts into one

--- scripts/load-unity-files.sh
+++ scripts/load-unity-files.sh
@@ -1,7 +1,7 @@
-cd ../public
-rm -rf unity
-mkdir unity
+echo "Fresh new public/unity folder"
+rm -rf ../public/unity; mkdir ../public/unity
 cd ../../
-rm -rf stt-unity
-git clone https://github.com/OutcomeLife/stt-unity.git
+echo "Clone or Pull the latest stt-unity"
+git clone https://github.com/OutcomeLife/stt-unity.git || (cd stt-unity; git pull)
+echo "Copy build files from stt-unity to alyson-v7"
 cp -R stt-unity/safeTrafficTown/Build/* alyson-v7/public/unity/
--- scripts/pull-unity-files.sh
+++
@@ -1,7 +0,0 @@
-cd ../public
-rm -rf unity
-mkdir unity
-cd ../../stt-unity
-git pull
-cd ..
-cp -R stt-unity/safeTrafficTown/Build/* alyson-v7/public/unity/
