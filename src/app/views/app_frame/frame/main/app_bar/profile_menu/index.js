428d088b898189ad68e83bfea86f1cec51005c42
sjlevens <sjlevens@gmail.com>
2020-07-10 13:09:00 +1000

    avatar-grp-test-ids

--- src/app/views/app_frame/frame/main/app_bar/profile_menu/index.js
+++ src/app/views/app_frame/frame/main/app_bar/profile_menu/index.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react'
 import { Menu, MenuItem, IconButton, Avatar } from '@material-ui/core'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 
-const ProfileMenu = ({ setViewing, profilePictureURL, userFullName, fakePictureForDemo, group }) => {
+const ProfileMenu = ({
+  setViewing,
+  profilePictureURL,
+  userFullName,
+  fakePictureForDemo,
+  group,
+}) => {
   const [menu, setMenu] = useState(null)
   return (
     <div>
-      <IconButton onClick={event => setMenu(event.currentTarget)} color="inherit" test-id={group.questionCode}>
+      <IconButton
+        onClick={event => setMenu(event.currentTarget)}
+        color="inherit"
+        test-id={group.questionCode}
+      >
         {fakePictureForDemo || (profilePictureURL && profilePictureURL.length) ? (
           <Avatar alt={userFullName} src={fakePictureForDemo || profilePictureURL} />
         ) : (
@@ -15,7 +25,7 @@ const ProfileMenu = ({ setViewing, profilePictureURL, userFullName, fakePictureF
       </IconButton>
       <Menu open={!!menu} anchorEl={menu} onClose={() => setMenu(null)}>
         <MenuItem
-          test-id='QUE_AVATAR_PROFILE_GRP'
+          test-id="QUE_AVATAR_PROFILE_GRP"
           onClick={() =>
             setViewing({
               view: 'FORM',
@@ -25,9 +35,11 @@ const ProfileMenu = ({ setViewing, profilePictureURL, userFullName, fakePictureF
           }
         >{`Profile`}</MenuItem>
         <MenuItem
+          test-id="QUE_AVATAR_SETTINGS"
           onClick={() => setViewing({ code: 'QUE_AVATAR_SETTINGS', parentCode: 'QUE_AVATAR_GRP' })}
         >{`Settings`}</MenuItem>
         <MenuItem
+          test-id="QUE_AVATAR_LOGOUT"
           onClick={() => setViewing({ code: 'QUE_AVATAR_LOGOUT', parentCode: 'QUE_AVATAR_GRP' })}
         >{`Logout`}</MenuItem>
       </Menu>
