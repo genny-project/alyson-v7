97ac926f82fd8d6f82580e461e5086a0b637df38
sjlevens <sjlevens@gmail.com>
2020-07-10 14:08:59 +1000

    hook-up-on-edit

---
+++ src/app/views/app_frame/frame/main/views/details/helpers/make-on-edit.js
@@ -0,0 +1,26 @@
+import Bridge from '../../../../../../../../utils/vertx/Bridge'
+import { includes, keys, compose, find, replace } from 'ramda'
+
+const getType = compose(
+  type => `QUE_EDIT_${type}_GRP`,
+  replace('PRI_IS_', ''),
+  find(includes('PRI_IS_')),
+  keys,
+)
+
+const makeOnEdit = ({ detailView, setViewing }) => () => {
+  Bridge.sendFormattedEvent({
+    parentCode: 'QUE_EDIT_ITEMS_GRP',
+    rootCode: 'QUE_EDIT_ITEMS_GRP',
+    code: getType(detailView),
+    targetCode: getType(detailView),
+    name: 'Edit',
+    attributeCode: 'QQQ_QUESTION_GROUP',
+    eventType: 'BTN_CLICK',
+    messageType: 'BTN',
+  })
+
+  setViewing({ view: 'FORM' })
+}
+
+export default makeOnEdit
