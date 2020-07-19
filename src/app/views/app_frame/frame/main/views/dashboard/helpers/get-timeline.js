468ae111739b86a566c305f2d7617c67f409f3d9
c7yrus <c7yrus@gmail.com>
2020-07-20 09:57:34 +1000

    indenting

--- src/app/views/app_frame/frame/main/views/dashboard/helpers/get-timeline.js
+++ src/app/views/app_frame/frame/main/views/dashboard/helpers/get-timeline.js
@@ -1,6 +1,14 @@
-import {map, path, prop} from 'ramda'
+import { map, path } from 'ramda'
 
-const getTimeline = (asks, user) =>
-  map(({name, question, questionCode, childAsks})=>({header: name, parentCode:questionCode, body:map(({name, attributeCode, question}) => ({content:name, status:user.attributes[attributeCode].valueBoolean, attributeCode: attributeCode, code:path(['code'], question)}), childAsks)}))(path(['QUE_DASHBOARD_TIMELINE_GRP', 'childAsks'], asks))
+const getTimeline = ( asks, user ) =>
+  map(({ name, questionCode, childAsks }) => ({
+    header: name,
+    parentCode: questionCode,
+    body: map(({ name, attributeCode, question }) => ({
+      content: name,
+      status: user.attributes[attributeCode].valueBoolean,
+      attributeCode: attributeCode,
+      code: path( ['code'], question ) }), childAsks ) }))
+  ( path( ['QUE_DASHBOARD_TIMELINE_GRP', 'childAsks'], asks ))
 
 export default getTimeline
